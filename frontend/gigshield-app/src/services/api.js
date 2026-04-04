e104aa73c047eda188eb2185645e6db8119125d0
Vibha Rajput <23bcs11151@cuchd.in>
2026-04-04 22:58:23 +0530

    fix frontend api base url

--- frontend/gigshield-app/src/services/api.js
+++ frontend/gigshield-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://guidewired-devtrails-it-wizards.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
